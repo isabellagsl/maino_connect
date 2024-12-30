3655116f246eefab3582ed0758ac73d100b7f3fc
Isabella Gonçalves <isabellagsoftware@gmail.com>
2024-12-30 15:44:12 -0300

    add update and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_post , only: %i[ show edit update destroy ]
+
   def index
     @posts = Post.all
   end
 
 
   def show
-    @post = Post.find(params[:id])
   end
-
 
   def create
     current_user = Current.user
     @post = Post.new(user: current_user, title: post_params[:title], content: post_params[:content])
     if @post.save
-      redirect_to @post, notice: "Post was successfully created."
+      redirect_to @post, notice: "Post was successfully created!"
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -32,14 +28,25 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_url, notice: "Post was successfully destroyed!"
   end
 
   private
     def post_params
       params.expect(post: [ :title, :content ])
+    end
+
+    def set_post 
+      @post = Post.find(params[:id])  
     end
 
 end
