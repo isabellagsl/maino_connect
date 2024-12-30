acfa33b21cc402d37399666baa2c56ee7ebd57b0
Isabella Gonçalves <isabellagsoftware@gmail.com>
2024-12-30 17:35:34 -0300

    profile edit

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,6 +1,5 @@
 class PostTag < ApplicationRecord
     has_many :post_taggings, dependent: :destroy
     has_many :posts, through: :post_taggings
-
-    post_tab.rb: validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
 end
