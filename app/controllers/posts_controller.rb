82bc67672bd71e1c8ffcda92bef6f99ffcd08ad3
Isabella Gonçalves <isabellagsoftware@gmail.com>
2025-01-02 16:07:55 -0300

    post tags create delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
   def index
-    page = params[:page].to_i || 1
+    page = (params[:page] || 1).to_i
     @per_page = 3
     offset = (page - 1) * @per_page
     @posts = Post.offset(offset).limit(@per_page).order(created_at: :desc)
@@ -16,6 +16,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(user: Current.user, title: post_params[:title], content: post_params[:content])
     if @post.save
+      post_tags = get_post_tags_from_params
+      add_post_tags(@post, post_tags)
       redirect_to @post, notice: "Post was successfully created!"
     else
       render :new, status: :unprocessable_entity
@@ -32,6 +34,12 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
+      post_tags = get_post_tags_from_params
+      current_tags = @post.post_tags.map(&:name)
+      new_tags = post_tags - current_tags
+      removed_tags = current_tags - post_tags
+      add_post_tags(@post, new_tags)
+      remove_post_tags(@post, removed_tags)
       redirect_to @post, notice: "Post was successfully updated!"
     else
       render :edit, status: :unprocessable_entity
@@ -44,11 +52,32 @@ class PostsController < ApplicationController
   end
 
   private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
     def post_params
       params.expect(post: [ :title, :content ])
     end
 
-    def set_post
-      @post = Post.find(params[:id])
+    def post_tags_params
+      params.require(:post).permit(:post_tags)
+    end
+
+    def get_post_tags_from_params
+      post_tags_params[:post_tags].split(",")
+    end
+
+    def add_post_tags(post, post_tags)
+      post_tags.each do |post_tag|
+        tag = PostTag.find_or_create_by(name: post_tag)
+        PostTagging.create(post: post, post_tag: tag)
+      end
+    end
+
+    def remove_post_tags(post, post_tags)
+      post_tags.each do |post_tag|
+        PostTagging.where(post: post, post_tag: PostTag.find_by(name: post_tag)).destroy_all
+      end
     end
 end
